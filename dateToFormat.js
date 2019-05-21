2beccaa4c22e76d1a74918e39c7f9c831021267d
Jona Neumeier <jona.neumeier@gmail.com>
2019-05-21 21:07:53 +0200

    Finished functionality. TODO: write readme.md and put in all possible formats

--- dateToFormat.js
+++ dateToFormat.js
@@ -1,52 +1,52 @@
-const vscode = require('vscode');
-const moment = require('moment');
+const vscode = require("vscode");
+const moment = require("moment");
 
-
-const checkDateString = (selectedString) => {
-    // console.log(selectedString);
+const checkDateString = () => {
+    const editor = vscode.window.activeTextEditor;
+    const selectionRange = editor.selection;
+    const selectedString = editor.document.getText(selectionRange);
 
     var validFormats = [];
+    var noFittingFormat = false;
 
     var check = false;
-    for(var i in formats){
+    for (var i in formats) {
         check = moment(selectedString, formats[i], true).isValid();
-        if(check){
-            // console.log('Valid');
+        if (check) {
             validFormats.push(formats[i]);
-        } else {
-            // console.log('Not Valid');
         }
     }
-    // console.log(validFormats);
 
-    const value = vscode.window.showQuickPick(['explorer', 'search', 'scm', 'debug', 'extensions'], { placeHolder: 'Select the view to show when opening a window.' })
-    .then(() => console.log(value));
+    // no fitting format
+    if (validFormats.length < 1) {
+        noFittingFormat = true;
+        validFormats.push("No fitting format found.");
+    }
 
-    const textEditor = vscode.window.activeTextEditor;
+    vscode.window
+        .showQuickPick(validFormats, {
+            placeHolder: "Select your format."
+        })
+        .then(selectedFormat => {
+            if (!noFittingFormat) {
+                editor.edit(builder => {
+                    builder.replace(editor.selection, selectedFormat);
+                });
+            }
+        });
+};
 
-    textEditor.edit((builder) => {
-        // todo: get the range of the selected text
-        builder.replace(new vscode.Range(0,0,1,0), 'Just replaced');
-    });
-
-    
-
-
-
-
-    // handle if array is empty
-
-    // use quickpick to show options https://code.visualstudio.com/api/references/vscode-api#window.showQuickPick
-}
-
-// Todo: Add more formats
+// dividers / |
 const formats = [
-    'DD-MMM-YYYY', 
-    'DD-MMM-YYYY HH:mm a',
-    'DD-MM-YYYY'
+    "DD-MMM-YYYY",
+    "DD-MMM-YYYY HH:mm a",
+    "DD-MM-YYYY",
+    "YYYY",
+    "YY",
+    "MM",
+    "DD-MM-YYYY",
+    "MM-DD-YYYY",
+    "MM||DD"
 ];
-
-
-
 
 exports.checkDateString = checkDateString;
--- extension.js
+++ extension.js
@@ -1,45 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-const dtf = require('./dateToFormat.js');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const vscode = require("vscode");
+const dtf = require("./dateToFormat.js");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+    let disposable = vscode.commands.registerCommand(
+        "extension.date-to-dateformat",
+        function() {
+            dtf.checkDateString();
+        }
+    );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "date-to-format" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.date-to-dateformat', function () {
-		
-		const editor = vscode.window.activeTextEditor;
-		const selectedString = editor.document.getText(editor.selection);
-
-		
-
-		dtf.checkDateString(selectedString);
-
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};
